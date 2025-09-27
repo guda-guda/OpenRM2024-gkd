c956840b5a6d8de09b7ae247fdaccfc1d6ea6a08
Yangtianhao <y3314708477@qq.com>
2025-09-27 19:22:30 +0800

    更改main函数，定义需要显示的具体参数键名，强制指定显示目标参数

--- terminal/src/main.cpp
+++ terminal/src/main.cpp
@@ -18,7 +18,16 @@ int main(int argc, char** argv) {
         switch (option) {
             case 'd':
                 rm::term_init();
-                rm::dashboard(key_name);
+                //rm::dashboard(key_name);
+                // 定义需要显示的具体参数键名（而非模块标识），以下为修改部分
+                //------------------------------
+                std::vector<std::string> display_keys = {
+                "target yaw", "target pit",  // 保留原有显示的参数
+                "antitop predict delay","antitop predict error","ekf_yolo_angle_error",
+                "trackqueueV4 predict error:","trackqueueV4 predict delay:"// 添加新的显示参数
+                };
+                rm::dashboard(display_keys);  // 传入具体参数键名，而非模块标识
+                //-------------------------------
                 break;
             case 'h':
                 std::cout << "Usage: " << argv[0] << " [-d] [-h] [-i] [-v] [-o <arg> <arg> ... ]" << std::endl;
